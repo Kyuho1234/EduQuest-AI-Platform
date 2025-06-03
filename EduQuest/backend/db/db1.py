dbb3024f282633fa112d84dc67a651fa4feb55ed
KOOKGYUHO <113431904+Kyuho1234@users.noreply.github.com>
2025-06-03 18:17:34 +0900

    Delete EduQuest directory

--- EduQuest/backend/db/db1.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
-import os
-
-DB1_URL = f"postgresql+asyncpg://{os.getenv('DB1_USER')}:{os.getenv('DB1_PASSWORD')}@113.198.66.75:13229/{os.getenv('DB1_DATABASE')}"
-
-engine_db1 = create_async_engine(DB1_URL, echo=True)
-SessionDB1 = sessionmaker(engine_db1, class_=AsyncSession, expire_on_commit=False)
